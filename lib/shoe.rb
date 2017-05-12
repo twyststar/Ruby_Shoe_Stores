d09328715a7eee06f803325316596026801431a7
Starlynne <lewis.start0905@gmail.com>
2017-05-12 12:30:59 -0700

    Add call back to capitalize name on shoe.

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -8,6 +8,7 @@ class Shoe < ActiveRecord::Base
 
   before_destroy :kill_all
   before_save :make_money
+  before_save :cap_name
 
   def not_stores
     all_stores = Store.all()
@@ -19,6 +20,15 @@ private
 
   def kill_all
     self.stores.delete_all
+  end
+
+  def cap_name
+    name = self.name.split
+      name.each do |word|
+        word.capitalize!
+      end
+    name = name.join(' ')
+    self.name = name
   end
 
   def make_money
